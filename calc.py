35289b2d01d53a9b814928f2b1f34df7830743c2
Gabriel Bremenkamp <39781077+Gabrielbs182@users.noreply.github.com>
2018-11-28 00:14:19 -0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,40 +1,43 @@
-def dec2bin(a): # Calculadora que vai converter o indice da lista em binario.
-
+def dec2bin(a): #Função que converte o índiced de números inteiros da lista em binário
   i = 0
   result = ''
-  lista = []
+  lista = []  
+  #-------------------- Inicio do processo ---------------------#  
   while a>1:
     i = a%2
     lista.append (i)
     a = a//2
   if a == 1:
     lista.append (1)
-  while (len(lista))<8: # Deixar o indice sempre como um octeto.
+  #-------------------- Fim do processo -------------------------# 
+  while (len(lista))<8: #Preenche o octeto com zeros a esquerda
     lista.append(0)
-  lista.reverse()
-  for b in lista:  # Transformando a lista em uma string
+  lista.reverse() #Reverte a lista
+  for b in lista:  #transformando a lista em uma string
     result += str(b)
   return result
 
-#--------------------------------------------------------
+#---------------------------------------------------------------------
 
-def bin2dec(n): # Calculadora que transforma de binario para decimal
+def bin2dec(n): #Função que converte um número decimal em binário
   
   result = ""
   i,dec = 0,0
   lista = n
   lista = list(lista)
   lista.reverse()
+  #-------------------- Inicio do processo ---------------------#
   for x in (lista):
     if x == "1":
       dec += 2**i
     i += 1
+  #-------------------- Fim do processo -------------------------#
   result = str(dec)
   return result
 
-  #------------------------------------------
-
-def end(masc,ip): #Função que calcula o endereço da subrede e disponibiliza tanto em binario quanto decimal.
+#----------------------------------------------------------------------
+  
+def end(masc,ip): #Função que calcula o endereço da sub-rede e disponibiliza tanto em binário quanto decimal.
   endereco,resultado = "",""
   listamasc,listaip = [],[]
   for x in masc:
@@ -44,7 +47,7 @@ def end(masc,ip): #Função que calcula o endereço da subrede e disponibiliza t
   for z in range (len(listamasc)):    
     if listamasc[z] == "0":
       listaip[z] = "0"
-  for b in listaip:  #transformando a lista em uma string
+  for b in listaip:  #Transformando a lista em uma string
     endereco += str(b)
   lista = separar(endereco)
   for x in range (len(lista)):
@@ -54,13 +57,11 @@ def end(masc,ip): #Função que calcula o endereço da subrede e disponibiliza t
       resultado+=result+'.'
     else:
       resultado+=result
-
   return [endereco,resultado]
 
 #---------------------------------------------------------
 
-def broad(masc,ip): #Função que faz o mesmo (end) porem calcula o broadcast.
-
+def broad(masc,ip):#Função que faz o mesmo porem calcula o broadcast.
   broadcast,resultado = "",""
   listamasc,listaip = [],[]
   for x in masc:
@@ -76,18 +77,15 @@ def broad(masc,ip): #Função que faz o mesmo (end) porem calcula o broadcast.
   for x in range (len(lista)):
     a = str(lista[x])
     result = bin2dec(a)
-
     if x != 3: #Essa condicional evita que seja concatenado junto a string um ponto extra no final
       resultado+=result+'.'
     else:
       resultado+=result
-
   return [broadcast,resultado]
 
 #--------------------------------------------
 
-def separar(n): #Transforma uma string em lista de numeros inteiros.
-
+def separar(n): #Transforma uma string em lista de números inteiros.
   linha = ""
   lista = []
   n += "." 
@@ -97,13 +95,11 @@ def separar(n): #Transforma uma string em lista de numeros inteiros.
     else:
       mat = int(linha) #transformo o numero de str para int.
       lista.append(mat) #Adiciono a um vertice na lista em INT.
-      linha = "" #reinicio o valor da variavel linha.
+      linha = "" #reinicio o valor da 
   return lista
 
 #------------------------------------------------------------------
-
-def coletar(n,lista): #Faz a coleta dos dados e calcula.  
-
+def coletar(n,lista): #Função que coleta a entrada e devolve em binário string
   resultado = ""
   for x in range (len(lista)):
     a = lista[x]
@@ -115,47 +111,14 @@ def coletar(n,lista): #Faz a coleta dos dados e calcula.
   return resultado
 
 #---------------------------------------------
-
-def ipv4(): #Recebe o numero do ip e passar pela função da coleta, verifica se o numero é valido.
-
-  teste,b = 0,0 #Da valor inicial as variaveis
-  ip = "" #Da valor inicial a string
-  i = str(input("\nInforme o numero do IPV4: "))
-  while teste != 1:
-    lista = separar(i) #Chama a função separar
-    for x in range (len(lista)): #Inicia um loop de acordo com o len
-      if lista[x] < 0 or lista[x] > 255: #Condicional com limites
-        teste = 0 #Controle do loop
-        b = 1 #Controle da condicional a seguir
+def verificar(i,lista): #Recebe um número e verifica se o numero é valido
+  teste,b = 0,0 #da valor inicial as variaveis
+  if (len(lista)) != 4: #Verifica se tem a quantidade certa de vértices
+    b = 1
+  else:
+    for x in range (len(lista)): #inicia um loop de acordo com o len
+      if lista[x] < 0 or lista[x] > 255: #Condicional que verifica um valor mánimo e um valor máximo para a entrada
+        b = 1
       else:
-        teste = 1
-    if b == 1: #Condicional de informação invalida
-      print("\nInformação invalida")
-      i = str(input("\nInforme o numero do IPV4 Novamente: ")) #pede novamente o valor de i
-      teste = 0
-      b = 0
-  ip = coletar(i,lista)
-  return ip
-
-#-------------------------------------------------------------#
-
-def masc(): #Recebe o numero da mascara, passa pela função da coleta, verifica se o numero é valido.
-
-  teste,b = 0,0 #Valor inicial
-  masc = "" #Inicia o valor da string
-  g = str(input("\nInforme o numero da Máscara de sub-rede: ")) #Recebe o valor da mascara
-  while teste != 1:
-    lista = separar(g) #Chama a função separar
-    for x in range (len(lista)): #Loop de acordo com o len
-      if lista[x] < 0 or lista[x] > 255:
-        teste = 0 #Variavel de controle
-        b = 1 #Variavel de controle
-      else:
-        teste = 1
-    if b == 1: #Condicional que detecta informação invalida
-      print("\nInformação invalida")
-      g = str(input("\nInforme novamente o numero da Máscara de sub-rede:")) #Pede novamente o valor da mascara.
-      teste = 0
-      b = 0
-  masc = coletar(g,lista)
-  return masc
+        b = 0
+  return b #Retorna o valor de (b), se o valor de (b) for = 1 então ele informa que (i) não é uma informação valida, se (b) for = 0, então ele informa que é valida
